320c8e8d87c6b4b2a5e6c207b2ba03befcffa3c4
alicemist <alicemozkara@gmail.com>
2023-04-20 18:37:34 +0300

    graphql needed

--- .github/scripts/send_glass_tasks_email.py
+++ .github/scripts/send_glass_tasks_email.py
@@ -1,52 +1,74 @@
+
 import os
-import smtplib
+
 import requests
+
 from email.message import EmailMessage
 
 def get_glass_tasks():
-    # GitHub API ile projenizdeki taskları alın
-    # Örnek olarak, projenizin "Glass" adında bir hedefi olduğunu varsayalım
+    token = os.environ.get("GITHUB_TOKEN", "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")
+    headers = {
+        "Accept": "application/vnd.github.inertia-preview+json",
+        "Authorization": f"Bearer {token}",
+    }
+
+    owner = "alicemist"
+    repo = "alicemist"
+    url = f"https://api.github.com/repos/{owner}/{repo}/projects"
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    projects = response.json()
+    print(projects)
+    glass_project = next((project for project in projects if project["name"] == "Glass"), None)
+
+    if glass_project is None:
+        print("Glass project not found.")
+        return []
+
+    project_id = glass_project["id"]
+    columns_url = f"https://api.github.com/projects/{project_id}/columns"
+    columns_response = requests.get(columns_url, headers=headers)
+    columns_response.raise_for_status()
+
+    columns = columns_response.json()
+
+    task_list = []
+    for column in columns:
+        column_id = column["id"]
+        cards_url = f"https://api.github.com/projects/columns/{column_id}/cards"
+        cards_response = requests.get(cards_url, headers=headers)
+        cards_response.raise_for_status()
+
+        cards = cards_response.json()
+        for card in cards:
+            task_list.append(card["note"])
+
+    return task_list
+
+def send_github_notification(task_list):
     user = "alicemist"
-    token = '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' #os.environ["GITHUB_TOKEN"]
-    url = f"https://api.github.com/users/{user}/projects"
+    repo = "alicemist"
+    token ='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'# os.environ["GITHUB_TOKEN"]
+
+    issue_title = "Your Daily Glass Tasks"
+    issue_body = f"Here are your daily Glass tasks:\n\n{task_list}"
+    url = f"https://api.github.com/repos/{user}/{repo}/issues"
     headers = {
         "Accept": "application/vnd.github+json",
         "Authorization": f"Bearer {token}",
     }
+    payload = {
+        "title": issue_title,
+        "body": issue_body,
+    }
 
-    projects = requests.get(url, headers=headers).json()
-    print("GitHub API Response:", projects)
-    glass_project = next((project for project in projects if project["name"] == "Glass"), None)
-    
-    if glass_project is None:
-        return "No Glass project found."
-
-    tasks_url = glass_project["cards_url"]
-    tasks = requests.get(tasks_url, headers=headers).json()
-
-    task_list = []
-    for task in tasks:
-        task_list.append(task["note"])
-
-    return "\n".join(task_list)
-
-def send_email(task_list):
-    email_username =os.environ["EMAIL_USERNAME"]
-    email_password = os.environ["EMAIL_PASSWORD"]
-    email_recipient = os.environ["EMAIL_RECIPIENT"]
-
-    msg = EmailMessage()
-    msg.set_content(f"Here are your daily Glass tasks:\n\n{task_list}")
-
-    msg["Subject"] = "Your Daily Glass Tasks"
-    msg["From"] = email_username
-    msg["To"] = email_recipient
-
-    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    server.login(email_username, email_password)
-    server.send_message(msg)
-    server.quit()
+    response = requests.post(url, json=payload, headers=headers)
+    if response.status_code == 201:
+        print("GitHub notification created successfully")
+    else:
+        print(f"Error creating GitHub notification: {response.status_code} - {response.text}")
 
 if __name__ == "__main__":
-    task_list = get_glass_tasks()
-    send_email(task_list)
+    get_glass_tasks()
+    
